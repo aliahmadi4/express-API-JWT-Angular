c1739f854e31b76ac96e2829cb19ee29f3688469
alimohammadahmadi <alimohammad.ahmadi2008@gmail.com>
2019-10-20 10:28:30 -0500

    sign in added for API

--- API/routes/users.js
+++ API/routes/users.js
@@ -13,29 +13,48 @@ router.get('/',async (req,res)=>{
 router.post('/signup', async (req,res)=>{
     try{
         const hash = await bcrypt.hash(req.body.password, 10);
-        
         const user = new User({
             name: req.body.name,
             email: req.body.email,
             password: hash
         })
         const user1 = await user.save();
-        jwt.sign({
-            name: req.body.name,
-            email: req.body.email,
-            password: hash
-        }, key.key,(err,data)=>{
-            if(err) throw err;
-            res.header('jwt', data);
-            res.json(user1);
-        })
+        res.json({msg: "You are registered!"})
         
     }catch(err){
-        res.json(err);
+        res.json({err});
     }
 })
 
-
+router.post('/login', async (req,res)=>{
+    console.log(`${req.body.email}  ${req.body.password}`)
+    if(!req.body.email || !req.body.password){
+        res.json({msg: "please provide email & password"});
+    }
+    try{
+        const hash = await bcrypt.hash(req.body.password,10);
+        const user = await User.findOne({email: req.body.email}).exec();
+        const result = await bcrypt.compare(req.body.password, user.password)
+        if(result){
+            jwt.sign({
+                name: user.name,
+                email: user.email,
+                password: user.password
+            }, key.key, (err,data)=>{
+                if(err) throw err;
+                res.header('jwt', data);
+                res.json(user);
+            })
+        }else{
+            res.json({msg: "email or password does not match!"})
+        }
+        
+    }catch(err){
+        throw err;
+    }
+    
+    
+})
 
 
 module.exports = router;
